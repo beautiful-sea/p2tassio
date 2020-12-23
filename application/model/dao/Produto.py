855019caed49ca1fbd0a6cd450de52e74bcf5627
Lindomar <lindomar1016@hotmail.com>
2020-12-22 21:07:15 -0300

    Feat:Model do Json de produtos criado

---
+++ application/model/dao/Produto.py
@@ -0,0 +1,15 @@
+import json
+from application.model.entity.Produtos import Produto
+
+with open ('application\\products.json') as product_file:
+        product_list = json.load(product_file)
+
+class produtoDAO:
+    def __init__(self):
+        self._produtos = []
+
+        for produto in product_list:
+             self._produtos.append(Produto(produto['id'], produto['image'], produto['name'], produto['description'], produto['oldPrice'], produto['price'], produto['installments']['count'], produto['installments']['value']))
+
+    def get_produtos(self):
+        return self._produtos
